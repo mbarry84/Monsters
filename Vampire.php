e45a7087eed1d755f40c118f8b281568df3500c3
Matthew Barry <mbarry5@mail.greenriver.edu>
2019-02-06 15:11:51 -0500

    initial commit Vampire class

--- Vampire.php
+++ Vampire.php
@@ -3,8 +3,10 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-class Dog extends Pet{
-    function fetch(){
+class Vampire extends Monster
+{
+    function fetch()
+    {
         echo "<p>".$this->getName()." is fetching.</p>";
     }
 }
