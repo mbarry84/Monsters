85a92e6a1e19bc3e2f067e26a6a2edecff499195
Guangpeng Wu <Gwu2@mail.greenriver.edu>
2019-02-06 14:54:48 -0500

    Monster finish

--- Monster.php
+++ Monster.php
@@ -1,49 +1,27 @@
 <?php
 
-class Monster{
+class Monster
+{
     private $_name;
-    private $_color;
 
-    //default constructor
-//    function __constructor(){
-//        $this->_name="unknown";
-//        $this->_color="?";
-//}
-
-    function __construct($name='unknown',$color='?'){
-        $this->_name=$name;
-        $this->_color=$color;
+    function __construct($name = 'unknown')
+    {
+        $this->_name = $name;
     }
 
-
-
-
-    function eat(){
-        echo "<p>".$this->_name."is eating</p>";
+    function setName($name)
+    {
+        $this->_name = $name;
     }
 
-    function talk(){
-        echo "<p>".$this->_name."is talking</p>";
-    }
-
-    function sleep(){
-        echo "<p>".$this->_name."is sleeping</p>";
-    }
-
-
-    function getName(){
+    function getName()
+    {
         return $this->_name;
     }
 
-    function setName($name){
-        $this->_name=$name;
+    function attack()
+    {
+        echo "<p>" . $this->_name . "is attacking</p>";
     }
 
-    function getColor(){
-        return $this->_color;
-    }
-
-    function setColor($color){
-        $this->_color=$color;
-    }
 }
