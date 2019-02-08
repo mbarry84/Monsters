36cf1f569634e7b2955765d947c0b0adabea9d69
Matthew Barry <mbarry5@mail.greenriver.edu>
2019-02-08 10:38:37 -0800

    Add files via upload
    Added zombie testing

--- index.php
+++ index.php
@@ -6,20 +6,29 @@ error_reporting(E_ALL);
     //Include the class definition
     require('Monster.php');
     require('Vampire.php');
+    require('zombie.php');
 
 
-    //$monster1=new Monster("Wu");
+    $monster1=new Monster("Wu");
 
 
-    //print_r($monster1);
+    print_r($monster1);
 
 
-    //$monster1->setName("Guangpeng");
-    //$monster1->getName();
-    //$monster1->attack();
+    $monster1->setName("Guangpeng");
+    $monster1->getName();
+    $monster1->attack();
 
-    //$vampire1=new Vampire("Li");
+    $vampire1=new Vampire("Li");
 
-//    $vampire1->Vampire();
-//    $vampire1->attack();
-//    $vampire1->getVictims();
+    $vampire1->attack();
+    $vampire1->getVictims();
+
+$vampire2=new Vampire("Wang");
+
+$vampire2->attack();
+$vampire2->getVictims();
+
+	$zombie1 = new zombie("Bob", 5);
+	$zombie1->attack();
+	$zombie1->getSpeed();
